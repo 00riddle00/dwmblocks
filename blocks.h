320c31f0a6bf4ad5b12f0025bf3b6deec9274afd
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2021-08-01 22:09:00 +0300

    Merge UPSTREAM

--- blocks.h
+++ blocks.h
@@ -1,39 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/   /*Command*/   /*Update Interval*/   /*Update Signal*/
-	
-	{" ", "~/.local/bin/statusbar/mediaplayer",    0,		10},
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	//{"", "~/.local/bin/statusbar/volume",		    0,		11},
-    
-	{" ", "~/.local/bin/statusbar/brightness",	    0,		12},
-
-	{" ", "~/.local/bin/statusbar/disk",			10,		0},
-
-	{" ", "~/.local/bin/statusbar/cpu-temp",		10,		0},
-
-	{" ", "~/.local/bin/statusbar/cpu-usage",		10,		0},
-
-	{" ", "~/.local/bin/statusbar/gpu-temp",		10,		0},
-
-	{"﬙ ", "~/.local/bin/statusbar/gpu-load",		10,		0},
-
-	{" ", "~/.local/bin/statusbar/memory",			10,		0},
-     
-	//{" ", "~/.local/bin/statusbar/wifi",			10,		0},
-
-	{"", "~/.local/bin/statusbar/battery",			10,		0},
-
-	{" ", "~/.local/bin/statusbar/calendar",		18000,	0},
-
-	//{" ", "~/.local/bin/statusbar/weather",		18000,	0},
-
-	{"", "~/.local/bin/statusbar/time",				60,		0},
-
-	{"", "~/.local/bin/statusbar/vpn",				5,		0},
-
-	{" ", "~/.local/bin/statusbar/arch-update",	5,		0},
+	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim = ' ';
+static char delim[] = " ";
+static unsigned int delimLen = 1;
